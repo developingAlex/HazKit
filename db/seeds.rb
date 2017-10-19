f366e1e4399651c2fae23217db3b81d3dde1ac79
Alex Palma <alex-palma@outlook.com>
2017-10-19 22:31:43 +1100

    Add user profiles to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#create dummy test users
+#create dummy test users:
 user1 = User.create!(email:'user1@mail.com', password:'password123', password_confirmation:'password123')
 user2 = User.create!(email:'user2@mail.com', password:'password123', password_confirmation:'password123')
 user3 = User.create!(email:'user3@mail.com', password:'password123', password_confirmation:'password123')
+
+#create dummy profiles for test users:
+profile1 = Profile.create!(user: user1, home_location: 'Batemans Bay, NSW, Australia', name: 'Henry Ford')
+profile2 = Profile.create!(user: user2, home_location: 'Melbourne, VIC, Australia', name: 'Ada Lovelace')
+profile3 = Profile.create!(user: user3, home_location: 'Yirrkala, NT, Australia', name: 'Alan Turing')
